ac6683a59e807a8e168f5ad8282b4040825f451e
mak013 <013.mike@gmail.com>
2024-03-26 11:00:35 +0400

    l3t1

---
+++ lessons/lesson3/task1/Program.cs
@@ -0,0 +1,9 @@
+﻿double CalculateFormula(int a, int b, int c, int d)
+{
+    double numenator = a*b;
+    int denominator = c + d;
+    double result = numenator / denominator;
+    return result;
+}
+CalculateFormula(1, 2, 3, 4);
+Console.WriteLine(CalculateFormula(1, 2, 3, 4)); 
